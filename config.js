b05a24ddaeb2f8d5964e02a44e6c1c80fa6f6f2e
downestj <tjdownes6@gmail.com>
2025-03-11 20:22:33 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.PorfolioConfig = {
-  name: 'John Doe',
-  jobTitle: 'Full-stack Web Developer',
+  name: 'TJ Downes',
+  jobTitle: 'Ethical Hacker',
   jobDescription: 'Sit quis in tempor quis. Nisi ex exercitation in irure. Reprehenderit sint aliqua et dolore enim laboris dolore exercitation magna aliqua ex ad.',
   skills: [
     {
