c8bf0e303eefade641057a7e54269e127a430275
downestj <tjdownes6@gmail.com>
2025-03-11 22:06:11 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,38 +2,28 @@ window.PorfolioConfig = {
   name: 'TJ Downes',
   jobTitle: 'Ethical Hacker | Cybersecurity Professional | Blockchain Enthusiast',
   jobDescription: 'Sit quis in tempor quis. Nisi ex exercitation in irure. Reprehenderit sint aliqua et dolore enim laboris dolore exercitation magna aliqua ex ad.',
- skills: [
+  skills: [
     { name: "Ethical Hacking", description: "Penetration testing and vulnerability analysis." },
     { name: "Blockchain", description: "Smart contracts and decentralized apps." }
   ],
-   education: [
+  education: [
     { name: "University of Arizona", description: "Masters of Science in Cybersecurity<br>3.95 GPA", link: "http://example.com" },
     { name: "Western Michigan University", description: "BBA in Computer Information Systems<br>3.8 GPA", link: "http://example.com" }
   ],
-  
   certifications: [
-    { name: "Certified Ethical Hacker", description: "asdfadf"},
-    { name: "CompTIA Pentest+", description: "adsadf"},
+    { name: "Certified Ethical Hacker", description: "" },
+    { name: "CompTIA Pentest+", description: "" },
     { name: "CompTIA Security+", description: "Blockchain technology and implementation." },
-    { name: "CompTIA Network+", description: "asdfa" }
+    { name: "CompTIA Network+", description: "" }
   ],
   contacts: {
     email: 'john.doe@gmail.com',
     mobileNo: '+639171234567',
     phoneNo: '+12 345 6789',
     links: [
-      {
-        name: 'GitHub',
-        link: 'https://github.com/jofftiquez'
-      },
-      {
-        name: 'Facebook',
-        link: 'https://facebook.com/jofftiquez'
-      },
-      {
-        name: 'Twitter',
-        link: 'https://twitter.com/jrtiquez'
-      }
+      { name: 'GitHub', link: 'https://github.com/jofftiquez' },
+      { name: 'Facebook', link: 'https://facebook.com/jofftiquez' },
+      { name: 'Twitter', link: 'https://twitter.com/jrtiquez' }
     ]
   }
 };
